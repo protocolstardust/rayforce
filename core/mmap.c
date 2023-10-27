f41fb75c343935b16396750e1724db0c2d9b4978
Anton <singaraiona@gmail.com>
2023-10-27 14:22:15 +0300

    feat(mmap): additional flags

--- core/mmap.c
+++ core/mmap.c
@@ -71,12 +71,12 @@ i64_t mmap_sync(nil_t *addr, u64_t size)
 
 nil_t *mmap_stack(u64_t size)
 {
-    return mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_STACK | MAP_GROWSDOWN, -1, 0);
+    return mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_STACK | MAP_POPULATE | MAP_NORESERVE | MAP_LOCKED | MAP_GROWSDOWN, -1, 0);
 }
 
 nil_t *mmap_malloc(u64_t size)
 {
-    return mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    return mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE | MAP_POPULATE | MAP_NORESERVE | MAP_LOCKED, -1, 0);
 }
 
 nil_t *mmap_file(i64_t fd, u64_t size)
